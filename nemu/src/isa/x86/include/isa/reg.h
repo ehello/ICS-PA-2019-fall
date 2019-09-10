c45a155f5bcd2acee8888956b0d4b074193acb92
tracer-ics2019 <tracer@njuics.org>
2019-09-10 09:34:15 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:34:15 up 29 min,  1 user,  load average: 0.52, 0.58, 0.59 179105a948f5056ecd8b62352a57f9f4fb7312b4

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,7 +17,6 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
- union{
   struct {
    union{	  
     uint32_t _32;
@@ -31,9 +30,9 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  
+  union{
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
- };
+  };
   vaddr_t pc;
 
 } CPU_state;
