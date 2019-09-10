da6feaf47d52ec3ba51636eb49a3b57a0ac8e5d2
tracer-ics2019 <tracer@njuics.org>
2019-09-10 19:29:36 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:29:36 up  5:33,  1 user,  load average: 0.16, 0.14, 0.13 6b25dbac91438b0fb3c85123667940477f0a6d7

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,11 +17,11 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+  union{
   struct {
-   union{	  
+  // union{	  
     uint32_t _32;
     uint16_t _16;
-   };
     uint8_t _8[2];
   // };
   } gpr[8];
@@ -33,7 +33,7 @@ typedef struct {
    */
   
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-  
+  };
   vaddr_t pc;
 
 } CPU_state;
