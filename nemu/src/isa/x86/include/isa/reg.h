92cde13cef97339d3fd917b1b74503268b0e38c0
tracer-ics2019 <tracer@njuics.org>
2019-09-10 14:53:01 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:53:01 up 56 min,  1 user,  load average: 0.47, 0.42, 0.38 d7671623a1f66269446875a23eb248c8c297326

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -35,7 +35,8 @@ typedef struct {
    * in PA2 able to directly access these registers.
    */
   
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  rtlreg_t eax;
+  rtlreg_t ecx, edx, ebx, esp, ebp, esi, edi;
  }; 
  
   vaddr_t pc;
