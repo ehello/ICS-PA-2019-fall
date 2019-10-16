24d0829a3ac9cae7dac11c02f6f11e995098e3fe
tracer-ics2019 <tracer@njuics.org>
2019-10-16 19:41:53 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:41:53 up 43 min,  1 user,  load average: 0.40, 0.44, 0.36 74feb9a779933f997c226d0274104e633b10ade3

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -74,7 +74,7 @@ static OpcodeEntry opcode_table [520] = {
   /* 0x4c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x50 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x54 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x55 */	EX(push), EMPTY, EMPTY, EMPTY,
+  ///* 0x55 */	EX(push), EMPTY, EMPTY, EMPTY,
   /* 0x58 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x5c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
