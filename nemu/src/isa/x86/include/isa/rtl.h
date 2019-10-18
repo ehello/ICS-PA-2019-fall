7c63905c27d667b51271798cb9797fe7e1fb1c01
tracer-ics2019 <tracer@njuics.org>
2019-10-18 20:56:53 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:56:53 up  6:56,  1 user,  load average: 0.48, 0.47, 0.42 740e56afebcc288eea268b9428c0cc9b64d2c8a0

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -36,7 +36,7 @@ static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
   //TODO();
-  *dest = vaddr_read(cpu.esp,4); 
+  *dest = vaddr_read(cpu.esp, 4); 
   cpu.esp += 4;
 
 }
