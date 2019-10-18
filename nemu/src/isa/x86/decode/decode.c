04b4fae085e4fd25c3537f3772d681d0ac31e2b4
tracer-ics2019 <tracer@njuics.org>
2019-10-18 22:01:34 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:01:34 up  8:00,  1 user,  load average: 0.68, 0.35, 0.29 eb77c54e2a6f1f44a44a5f3d8b5478f79f3d6b0a

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -30,7 +30,7 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
-  op->simm = instr_fetch(pc,op->width);//不完善，之后还需改进
+  op->imm = instr_fetch(pc,op->width);//不完善，之后还需改进
   rtl_li(&op->val, op->simm);
 
   print_Dop(op->str, OP_STR_SIZE, "$0x%x", op->simm);
