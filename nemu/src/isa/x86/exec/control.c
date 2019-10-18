04b0e9c5852f8799fb47e9b26f75a9d029380289
tracer-ics2019 <tracer@njuics.org>
2019-10-18 20:53:14 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:53:14 up  6:52,  1 user,  load average: 0.68, 0.43, 0.39 856c47c4c19fb16b9f43618aa59231deb8dc44f4

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -28,7 +28,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   //TODO();
   rtl_push(pc);
-  interpret_rtl_j(decinfo.jmp_pc);
+  //interpret_rtl_j(decinfo.jmp_pc);
 
 
   print_asm("call %x", decinfo.jmp_pc);
