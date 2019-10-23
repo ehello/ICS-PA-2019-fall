4c44b617d9c782b00ccbc923f2b1eb38db8d0d39
tracer-ics2019 <tracer@njuics.org>
2019-10-23 22:06:01 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:06:01 up  3:50,  1 user,  load average: 0.15, 0.51, 0.55 b407d64ec900daba492aa9adede906007f25cc55

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,12 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  //ref_difftest_getregs(ref_r);
+  if(ref_r->eax !=cpu.eax || ref_r->ecx != cpu.ecx
+      ||ref_r->edx != cpu.edx||ref_r->ebx != cpu.ebx
+      ||ref_r->esp != cpu.esp||ref_r->ebp != cpu.ebp
+      ||ref_r->esi != cpu.esi|| ref_r->edi != cpu.edi
+      ||pc != cpu.pc)
+    return true;
   
   return false;
 }
