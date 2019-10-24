5b29200f4855c9c8627f360905a25972a49f6324
tracer-ics2019 <tracer@njuics.org>
2019-10-24 23:25:21 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:25:21 up  9:26,  1 user,  load average: 0.14, 0.14, 0.17 6336550dc109f5ba8a489cebe1525c666c6ddb3b

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -39,7 +39,7 @@ char* strcat(char* dst, const char* src) {
 
 int strcmp(const char* s1, const char* s2) {
   assert((s1 != NULL)&&(s2 != NULL));
-  while (*s1&&*s2&&(*s1 == *s2)){
+  while (*s1 && *s2 && (*s1 == *s2)){
     s1++;
     s2++;
   }
