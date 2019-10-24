8268819ee0e2446bf4a7766f2cf9ae5afe9ffceb
tracer-ics2019 <tracer@njuics.org>
2019-10-24 20:19:43 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:19:43 up  6:20,  1 user,  load average: 0.68, 0.70, 0.62 69000706c3c7af3b301b2b2fe8ac42e95b19a883

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -7,7 +7,15 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       ||ref_r->esp != cpu.esp||ref_r->ebp != cpu.ebp
       ||ref_r->esi != cpu.esi|| ref_r->edi != cpu.edi
       ||pc != cpu.pc){
-        
+        printf("ref's eax is 0x%x\n",ref_r->eax);
+        printf("ref's ecx is 0x%x\n",ref_r->ecx);
+        printf("ref's edx is 0x%x\n",ref_r->edx);
+        printf("ref's ebx is 0x%x\n",ref_r->ebx);
+        printf("ref's esp is 0x%x\n",ref_r->esp);
+        printf("ref's ebp is 0x%x\n",ref_r->ebp);
+        printf("ref's esi is 0x%x\n",ref_r->esi);
+        printf("ref's edi is 0x%x\n",ref_r->edi);
+        printf("pc is 0x%x, cpu.pc is 0x%x\n",pc,cpu.pc);
         return false;
       }
     
