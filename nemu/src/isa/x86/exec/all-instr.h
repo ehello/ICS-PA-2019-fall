b516f3ee7a95c35bec1ed9ce7b1bc9b2f6468766
tracer-ics2019 <tracer@njuics.org>
2019-10-26 01:30:59 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  01:30:59 up  6:29,  1 user,  load average: 0.56, 0.33, 0.32 6352b8568f6b8711f08e07ff99bc127910b94df0

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -24,6 +24,7 @@ make_EHelper(or);
 make_EHelper(sar);
 make_EHelper(shl);
 make_EHelper(dec);
+make_EHelper(not);
 
 make_EHelper(operand_size);
 
--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -75,7 +75,7 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  TODO();
-
+  //TODO();
+  rtl_not(&id_dest->val,&id_dest->val);
   print_asm_template1(not);
 }
