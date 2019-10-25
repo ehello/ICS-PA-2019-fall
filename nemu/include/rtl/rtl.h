f5560809b23fd7ce52df54e36bc2408b5bd57fb2
tracer-ics2019 <tracer@njuics.org>
2019-10-25 22:31:14 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:31:14 up  3:29,  1 user,  load average: 0.39, 0.32, 0.18 900807862fe73d9c417abe8625f45f14582f9985

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -133,7 +133,7 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
   *dest = ~*src1;
-  TODO();
+  //TODO();
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
