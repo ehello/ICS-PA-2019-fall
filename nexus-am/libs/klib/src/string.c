e61222398165046e2f9e89c934c7d26eaec9a286
tracer-ics2019 <tracer@njuics.org>
2019-10-26 21:00:36 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:00:36 up 1 day,  1:58,  1 user,  load average: 0.14, 0.62, 0.78 5283d163f53ad7eca03c20869b041505bb6888f1

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,5 +1,4 @@
 #include "klib.h"
-
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
@@ -13,12 +12,9 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-  char* ret = dst;
+  char *ret = dst;
   assert((dst != NULL)&&(src != NULL));
-  while(*src != '\0'){
-    *dst++ = *src++;
-  }
-  *dst = '\0';
+  while((*dst++ = *src++) !='\0') ;
   return ret;
 }
 
@@ -28,16 +24,11 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  char* ret = dst;
+  char *ret = dst;
   assert((dst != NULL)&&(src != NULL));
-  while(*dst != '\0') 
-    dst++;
-  while (*src)
-  {
-    *dst = *src;
-    dst+=1;
-    src+=1;
-  }
+  while(*dst) dst+=1;
+  dst-=1;
+  while ((*dst++ = *src++)) ;
   return ret;
   
 }
@@ -48,11 +39,10 @@ int strcmp(const char* s1, const char* s2) {
     s1+=1;
     s2+=1;
   }
-  int ret = -2;
-  if (*s1-*s2<0) ret = -1;
-  else if (*s1-*s2>0) ret = 1;
-  else ret = 0; 
-  return ret;
+  if (*s1 == *s2) return 0;
+  else if (*s1>*s2) return 1;
+  return -1;
+  //return *s1-*s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -61,30 +51,30 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  assert(v != NULL);
-  char* ret = (char*) v;
+  char *ret = (char*)v;
+  assert(v != NULL || n>0);
   while (n-- > 0){
-    *ret++ = (char)c;
+    *ret++ = c;
   }
-  return ret;
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  //TODO();
+  
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  if (n<0) return 0;
-  while(n>0 && (*(char*)s1 == *(char*)s2)){
-    s1 = (char*)s1+1;
-    s2 = (char*)s2+1;
+  assert(s1 != NULL || s2 != NULL || n>0);
+  const char *str1 = (char*)s1;
+  const char *str2 = (char*)s2;
+  while(--n && (*str1 == *str2)){
+    str1+=1;
+    str2+=1;
   }
-  int ret = -2;
-  if ((*((unsigned char *)s1) - *((unsigned char *)s2))<0) ret = -1;
-  else if((*((unsigned char *)s1) - *((unsigned char *)s2))>0) ret = 1;
-  else ret = 0;
-  return ret;
+  if (*str1>*str2) return 1;
+  else if(*str1<*str2) return -1;
+  return 0;
 }
 
 #endif
