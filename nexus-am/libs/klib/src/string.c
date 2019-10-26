268b72bf8bca8d423950f320fefc1ffbd0fcca17
tracer-ics2019 <tracer@njuics.org>
2019-10-26 21:14:27 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:14:27 up 1 day,  2:12,  1 user,  load average: 0.40, 0.62, 0.70 cc7c4b262079886e741da83992dfb52206592b7

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -14,7 +14,7 @@ size_t strlen(const char *s) {
 
 char *strcpy(char* dst,const char* src) {
   char *ret = dst;
-  //assert((dst != NULL)&&(src != NULL));
+  assert((dst != NULL)&&(src != NULL));
   while((*dst++ = *src++) !='\0') ;
   return ret;
 }
@@ -26,15 +26,15 @@ char* strncpy(char* dst, const char* src, size_t n) {
 
 char* strcat(char* dst, const char* src) {
   char *ret = dst;
-  //assert((dst != NULL)&&(src != NULL));
-  while(*dst) dst+=1;
+  assert((dst != NULL)&&(src != NULL));
+  while(*dst) dst+=1;//注意写法不要混用
   while ((*dst++ = *src++)) ;
   return ret;
   
 }
 
 int strcmp(const char* s1, const char* s2) {
-  //assert((s1 != NULL)&&(s2 != NULL));
+  assert((s1 != NULL)&&(s2 != NULL));
   while (*s1 && *s2 && (*s1 == *s2)){
     s1+=1;
     s2+=1;
@@ -52,10 +52,8 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 
 void* memset(void* v,int c,size_t n) {
   char *ret = (char*)v;
-  //assert(v != NULL || n>0);
-  while (n--){
-    *ret++ = c;
-  }
+  assert(v != NULL || n>0);
+  while (n--) *ret++ = c;
   return v;
 }
 
@@ -67,7 +65,7 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  //assert(s1 != NULL || s2 != NULL || n>0);
+  assert(s1 != NULL || s2 != NULL || n>0);
   const char *str1 = (char*)s1;
   const char *str2 = (char*)s2;
   while(--n && (*str1 == *str2)){
@@ -77,6 +75,7 @@ int memcmp(const void* s1, const void* s2, size_t n){
   if (*str1>*str2) return 1;
   else if(*str1<*str2) return -1;
   return 0;
+  //return *str1-*str2;
 }
 
 #endif
