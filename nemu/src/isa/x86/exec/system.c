5d0b95195e6ff3af8d3599b6199b60d25454b0d7
tracer-ics2019 <tracer@njuics.org>
2019-10-30 19:20:55 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:20:55 up  1:01,  1 user,  load average: 0.42, 0.29, 0.27 44e2be3fe5c7262b6c236fa74fa896cf4baf5389

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -58,7 +58,7 @@ make_EHelper(out) {
   switch(id_dest->val){
     case 1: pio_write_b(id_dest->val, id_src->val); break;
     case 2: pio_write_w(id_dest->val, id_src->val); break;
-    case 3: pio_write_l(id_dest->val, id_src->val); break;
+    case 4: pio_write_l(id_dest->val, id_src->val); break;
   }
   print_asm_template2(out);
 }
