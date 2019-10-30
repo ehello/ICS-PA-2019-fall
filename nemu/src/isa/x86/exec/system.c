6daf210b213c5e1a7476d1d99cb5c15d61a6f6b2
tracer-ics2019 <tracer@njuics.org>
2019-10-30 19:21:45 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:21:45 up  1:02,  1 user,  load average: 0.28, 0.27, 0.27 e1709e2ed7f9bceb50b65fd2f4cce34c4ca0ca6

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -55,7 +55,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //TODO();
-  switch(id_dest->val){
+  switch(id_dest->width){
     case 1: pio_write_b(id_dest->val, id_src->val); break;
     case 2: pio_write_w(id_dest->val, id_src->val); break;
     case 4: pio_write_l(id_dest->val, id_src->val); break;
