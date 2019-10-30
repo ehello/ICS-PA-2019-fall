9711485a9674e2739a1322f984d7fa824be0107f
tracer-ics2019 <tracer@njuics.org>
2019-10-30 16:37:18 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:37:18 up 33 min,  1 user,  load average: 0.61, 0.94, 0.79 c96ce09fb7acb129d3d28502b7066e162313a22f

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -7,10 +7,10 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      uptime->hi = 0; 
-      uptime->lo = 0;
-      //uptime->hi = inl(0x48);
-      //uptime->lo = inl(0x48);
+      //uptime->hi = 0; 
+      //uptime->lo = 0;
+      uptime->hi = inl(0x48);
+      uptime->lo = inl(0x48);
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
