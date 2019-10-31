7ff9138497076a635e827fa742ee6f80417ac29d
tracer-ics2019 <tracer@njuics.org>
2019-10-31 17:23:41 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:23:41 up 17:10,  1 user,  load average: 0.20, 0.31, 0.56 a8f9f890ef75764c5ab4a80cc4bed7b25419ef05

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -87,14 +87,20 @@ make_EHelper(dec) {
 
 make_EHelper(neg) {
   //TODO();
-  if (id_dest->val == 0){
+  /*if (id_dest->val == 0){
     s0 = 0;
     rtl_set_CF(&s0);
   }
   else{
     s0 = 1;
     rtl_set_CF(&s0);
-  }
+  }*/
+  //s1 = -id_dest->val;
+  //operand_write(id_dest,&s1);
+  //rtl_update_ZFSF(&s1,id_dest->width);
+  //rlt_xor(&s0,&id_dest->val,&id_src->val);
+  //rlt_xor(&s1,&id_dest->val,);
+
   s0 = 0;
   rtl_sub(&s1,&s0,&id_dest->val);
   operand_write(id_dest,&s1);
