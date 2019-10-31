46149a8dbed50f0062c7e815e76cfea8a3e3a699
tracer-ics2019 <tracer@njuics.org>
2019-10-31 23:25:46 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:25:46 up 23:12,  1 user,  load average: 0.37, 0.22, 0.27 934e38e99204f9d823de55795ff6094caf4babb2

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -164,7 +164,7 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
   rtl_setrelop(relop, dest, src1, &ir);
 }
 
-static  void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
   rtl_shri(dest,src1,width*8-1);
   //TODO();
