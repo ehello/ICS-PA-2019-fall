31a9fa5f4fac0393e0b00449ba8d60d700ce48ad
tracer-ics2019 <tracer@njuics.org>
2019-10-31 17:10:09 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:10:09 up 16:56,  1 user,  load average: 0.25, 0.59, 0.93 74064acfadf2a6846c4232e9766dfd5b609f8d5b

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,13 +7,14 @@
 //#define SCREEN_H 300
 //#define SCREEN_W 400
 
+static int vga_data;
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      uint32_t vga_data = inl(SCREEN_ADDR);
-      info->width = vga_data;//todo?
-      info->height = vga_data;
+      //uint32_t vga_data = inl(SCREEN_ADDR);
+      info->width = vga_data >> 16;//todo?
+      info->height = vga_data & 0xFFFF;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
     
@@ -38,6 +39,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 
 void __am_vga_init() {
   int i;
+  vga_data = inl(SCREEN_ADDR);
   int size = screen_width() * screen_height();
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < size; i ++) fb[i] = i;
