6dc4977ebf2abac67df73b72403c81a4d81f8b6f
tracer-ics2019 <tracer@njuics.org>
2019-10-31 15:59:28 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:59:28 up 15:46,  1 user,  load average: 0.99, 1.28, 1.20 ee686e8f3eac64ce8d33e697d41e8943c2e5305b

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -31,8 +31,12 @@ static inline void update_screen() {
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
   //TODO();
-  if(offset!=0&&is_write) update_screen();
-  else assert(0);
+  if(offset == 0){
+    assert(!is_write);
+  }
+  else {
+    if(is_write) update_screen();
+  }
     
 
 }
