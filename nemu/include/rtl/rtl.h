190e98edbea9df924067de4578fb626fd88aed12
tracer-ics2019 <tracer@njuics.org>
2019-11-01 14:26:15 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:26:15 up 1 day, 14:12,  1 user,  load average: 0.66, 0.86, 0.82 96d40c199c1b784d5c57fff6ed9e408683eb02f

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -136,7 +136,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   //TODO();
 }
 
-void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   int32_t temp = (int32_t) *src1;
