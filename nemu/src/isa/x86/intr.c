ee11406d963135d97a7fc15827e6493f38c02eee
tracer-ics2019 <tracer@njuics.org>
2019-11-16 22:20:36 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:20:36 up  7:36,  1 user,  load average: 0.70, 0.49, 0.47 ceaf2beef902ec38500738496a80f857b97ddd0

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -9,13 +9,13 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eflags.val);
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
-  
+
   GateDesc gatedesc;
   gatedesc.offset_15_0= (vaddr_read(cpu.idtr.base+NO*8,4)& 0x0000ffff);//取低16位赋值给offset15_0
   gatedesc.offset_31_16 = ((vaddr_read(cpu.idtr.base+NO*8+4,4) & 0xffff0000)>>16);//取高16位赋值给offset31_16
   //以上赋值的原因在于GateDesc联合体的结构
   
-  cpu.eflags.IF = 0;
+  //cpu.eflags.IF = 0;
   
   vaddr_t goalAddr = gatedesc.offset_15_0|(gatedesc.offset_31_16<<16);
   rtl_j(goalAddr);
--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -7,8 +7,8 @@ struct _Context {
   uintptr_t  edi,esi,ebp,esp,ebx,edx,ecx,eax;
   struct _AddressSpace *as;
   int irq;
-  //uintptr_t eip,cs,eflags;
-  uintptr_t eflags,cs,eip;
+  uintptr_t eip,cs,eflags;
+  //uintptr_t eflags,cs,eip;
 };
 
 #define GPR1 eax
