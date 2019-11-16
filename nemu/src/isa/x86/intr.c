96499c230c3a81a3ed07059bd23acf0e8580861c
tracer-ics2019 <tracer@njuics.org>
2019-11-16 19:56:22 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:56:22 up  5:12,  1 user,  load average: 0.67, 0.48, 0.50 fe230cb917c8963da9208e13d5105447cf219269

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -13,8 +13,8 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   GateDesc gatedesc;
   gatedesc.offset_15_0 = vaddr_read(cpu.idtr.base+NO*8,4);
   gatedesc.offset_31_16 = vaddr_read(cpu.idtr.base+NO*8+4,4);
-  gatedesc.present =  (uint16_t)vaddr_read(cpu.idtr.base+NO*8,4) & 0b1000000000000000;
-  if (gatedesc.present == 0) panic("invalid");
+  //gatedesc.present =  (uint16_t)vaddr_read(cpu.idtr.base+NO*8,4) & 0b1000000000000000;
+  //if (gatedesc.present == 0) panic("invalid");
   
   //gatedesc.val = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
   vaddr_t goalAddr = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
