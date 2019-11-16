81d83a7299107401f54aad67b659f0f386ad6fc7
tracer-ics2019 <tracer@njuics.org>
2019-11-16 22:23:43 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:23:43 up  7:39,  1 user,  load average: 0.77, 0.69, 0.56 c6b8e0d158bc660e508829debbf7e0c110d2030c

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -7,6 +7,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   rtl_push(&cpu.eflags.val);
+  cpu.eflags.IF = 0;
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
 
@@ -15,7 +16,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   gatedesc.offset_31_16 = ((vaddr_read(cpu.idtr.base+NO*8+4,4) & 0xffff0000)>>16);//取高16位赋值给offset31_16
   //以上赋值的原因在于GateDesc联合体的结构
   
-  cpu.eflags.IF = 0;
+  //cpu.eflags.IF = 0;
   
   vaddr_t goalAddr = gatedesc.offset_15_0|(gatedesc.offset_31_16<<16);
   rtl_j(goalAddr);
