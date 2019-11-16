eab151156bdd202e950065417a598aaa296d676a
tracer-ics2019 <tracer@njuics.org>
2019-11-16 21:50:29 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:50:29 up  7:06,  1 user,  load average: 1.05, 0.68, 0.52 66bdbae0c1bd1c3fde847fc7711064e16d136dca

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -2,8 +2,8 @@
 #define __ARCH_H__
 
 struct _Context { 
-  //uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
-  uintptr_t  eip,cs,eflags,edi,esi,ebp,esp,ebx,edx,ecx,eax;
+  //uintptr_t  eip,cs,eflags,edi,esi,ebp,esp,ebx,edx,ecx,eax;
+  uintptr_t  edi,esi,ebp,esp,ebx,edx,ecx,eax,eip,cs,eflags;
   struct _AddressSpace *as;
   int irq;
 };
