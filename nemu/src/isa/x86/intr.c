14a98a19b4937c297414280a1db02b36571e920b
tracer-ics2019 <tracer@njuics.org>
2019-11-16 20:22:59 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:22:59 up  5:38,  1 user,  load average: 0.24, 0.29, 0.42 160f8d7590468f83a41b238054f8af3f75969a43

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -11,13 +11,13 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   rtl_push(&ret_addr);
 
   GateDesc gatedesc;
-  gatedesc.offset_15_0 = vaddr_read(cpu.idtr.base+NO*8,4);
-  gatedesc.offset_31_16 = vaddr_read(cpu.idtr.base+NO*8+4,4);
-  gatedesc.present = 1;
-  if (gatedesc.present == 0) panic("invalid");
-  
+  gatedesc.offset_15_0 = vaddr_read(cpu.idtr.base+NO*8,4)& 0x0000ffff;
+  gatedesc.offset_31_16 = (vaddr_read(cpu.idtr.base+NO*8+4,4) & 0xffff0000)<<16;
+  //gatedesc.present = 1;
+  //if (gatedesc.present == 0) panic("invalid");
+  cpu.eflags.IF = 0;
   //gatedesc.val = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
-  vaddr_t goalAddr = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
+  vaddr_t goalAddr = gatedesc.offset_15_0+gatedesc.offset_31_16;
   rtl_j(goalAddr);
 }
 
