ede3382e6a00e5d5802a1bcf36f5f43bcceb9b88
tracer-ics2019 <tracer@njuics.org>
2019-11-16 19:23:14 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:23:14 up  4:39,  1 user,  load average: 0.97, 1.10, 0.90 3e2318c35615300a36d77c4ffdbf3f0f9a709a2

--- nemu/src/isa/x86/init.c
+++ nemu/src/isa/x86/init.c
@@ -16,7 +16,7 @@ const long isa_default_img_size = sizeof(isa_default_img);
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = PC_START;
-  cpu.cs = 8;
+  cpu.cs = 0x8;
   cpu.eflags.val = 0x2;
 }
 
--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -16,8 +16,9 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   gatedesc.present =  (uint16_t)vaddr_read(cpu.idtr.base+NO*8,4) & 0b1000000000000000;
   if (gatedesc.present == 0) panic("invalid");
   
-  gatedesc.val = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
-  rtl_j(gatedesc.val);
+  //gatedesc.val = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
+  vaddr_t goalAddr = (gatedesc.offset_15_0 & 0x0000ffff)+(gatedesc.offset_31_16 & 0xffff0000);
+  rtl_j(goalAddr);
 }
 
 bool isa_query_intr(void) {
