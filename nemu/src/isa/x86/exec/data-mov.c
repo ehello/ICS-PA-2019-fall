ca66e8f1f6964cac59449008d2e862e768423050
tracer-ics2019 <tracer@njuics.org>
2019-11-17 11:20:17 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  11:20:17 up 20:36,  1 user,  load average: 0.68, 0.46, 0.32 85ee967926e8ac917cc7e02893266bc4fa5e10bd

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -42,7 +42,7 @@ make_EHelper(popa) {
   rtl_pop(&cpu.ebp);
   rtl_pop(&s1);
   rtl_pop(&cpu.ebx);
-  rtl_pop(&cpu.edx);
+  rtl_pop(&cpu.edx);//之前多打了一个edx，要细心呐
   rtl_pop(&cpu.ecx);
   rtl_pop(&cpu.eax);
 
--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -10,20 +10,14 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   cpu.eflags.IF = 0;
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
-  union {
-    GateDesc gatedesc;
-    struct{uint32_t lo,hi;};
-  } temp;
-
-  temp.lo = vaddr_read(cpu.idtr.base+NO*8,4);
-  temp.hi = vaddr_read(cpu.idtr.base+NO*8+4,4);
-  //GateDesc gatedesc;
-  //gatedesc.offset_15_0= (vaddr_read(cpu.idtr.base+NO*8,4)& 0x0000ffff);//取低16位赋值给offset15_0
-  //gatedesc.offset_31_16 = ((vaddr_read(cpu.idtr.base+NO*8+4,4) & 0xffff0000)>>16);//取高16位赋值给offset31_16
+  
+  GateDesc gatedesc;
+  gatedesc.offset_15_0= (vaddr_read(cpu.idtr.base+NO*8,4)& 0x0000ffff);//取低16位赋值给offset15_0
+  gatedesc.offset_31_16 = ((vaddr_read(cpu.idtr.base+NO*8+4,4) & 0xffff0000)>>16);//取高16位赋值给offset31_16
   //以上赋值的原因在于GateDesc联合体的结构
 
   
-  vaddr_t goalAddr = (temp.gatedesc.offset_15_0 & 0xffff)|((temp.gatedesc.offset_31_16 & 0xffff)<<16);
+  vaddr_t goalAddr = gatedesc.offset_15_0|(gatedesc.offset_31_16 & 0x0000ffff)<<16;
   rtl_j(goalAddr);
 }
 
