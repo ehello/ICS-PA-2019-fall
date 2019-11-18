b89796f9da5731eac620cd49f2a4685a35168c95
tracer-ics2019 <tracer@njuics.org>
2019-11-18 21:54:09 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:54:09 up  1:05,  1 user,  load average: 0.43, 0.45, 0.44 86b254d49656bd2f7e6190d4301bed1c859e3224

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -67,15 +67,15 @@ void* memset(void* v,int c,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  char *dst = (char*)out;
+  /*char *dst = (char*)out;
   char *src = (char*)in;
-  while(n>0) {*dst++ = *src++; n--;};
-  return out;
-  /*char *csrc = (char*)in, *cdest = (char*)out;
+  while(n--) {*dst++ = *src++;}
+  return out;*/
+  char *csrc = (char*)in, *cdest = (char*)out;
   for (int i = 0; i < n; i ++) {
     cdest[i] = csrc[i];
   }
-  return out;*/
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
