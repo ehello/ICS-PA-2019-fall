8f997fc5a5e76a4cac90d0047ce6cc30d75903ae
tracer-ics2019 <tracer@njuics.org>
2019-11-18 22:16:48 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:16:48 up  1:28,  1 user,  load average: 0.86, 0.49, 0.39 71f7fda8a5023508987e26825501ae12acc47c15

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char buff[4096];//原来设置为100，太小了，所以在PA3一开始报错了
+  char buff[65536];//原来设置为100，太小了，所以在PA3一开始报错了
   va_list args;
   int n;
 
@@ -24,7 +24,7 @@ int printf(const char *fmt, ...) {
 
 static char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
 char *number(char *str,long num,int base){// TODO: 精度实现
-  char tmp[1024];
+  char tmp[65536];
   char* dig = digits;
   int i = 0;
   if (num < 0) {
