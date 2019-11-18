371c1137e331eb43ce8594b3a257f2adc1052ae9
tracer-ics2019 <tracer@njuics.org>
2019-11-18 23:34:25 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:34:25 up  2:46,  1 user,  load average: 0.20, 0.32, 0.40 d328d79c787b4b62568413fdf8e3192ccf5c208f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,7 +24,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(ehdr,0,sizeof(Elf_Ehdr));
   printf("type is %d\n",temp.e_type);
   printf("entry is %d\n",temp.e_entry);
-  return (*ehdr).e_entry;
+  ramdisk_write((void*)temp.e_entry,0,get_ramdisk_size());
+  return temp.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
