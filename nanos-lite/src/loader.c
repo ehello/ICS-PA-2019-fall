2e9fb9c3bf91a9242375616b165a44c1362c67aa
tracer-ics2019 <tracer@njuics.org>
2019-11-19 15:06:40 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:06:40 up  2:30,  1 user,  load average: 0.53, 0.33, 0.37 d8b8c6161bfafc12d187212ae2a1ed91a0cd508

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,7 +37,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       printf("vaddr is %d,paddr is %d ",pht[i].p_vaddr,pht[i].p_paddr);
       printf(" and memsize is %d\n",pht[i].p_memsz);
       ramdisk_read((void*)pht[i].p_vaddr, pht[i].p_offset, pht[i].p_memsz);
-      //memset((void*)(pht[i].p_vaddr+pht[i].p_filesz),0,pht[i].p_memsz-pht[i].p_filesz);
+      memset((void*)(pht[i].p_vaddr+pht[i].p_filesz),0,pht[i].p_memsz-pht[i].p_filesz);
     }
   }
   //ramdisk_write((void*)temp.e_entry,temp1.p_vaddr,ge);
