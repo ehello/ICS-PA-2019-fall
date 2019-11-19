161823b432b834b7e26c45b26e4672341c112cf2
tracer-ics2019 <tracer@njuics.org>
2019-11-19 22:15:22 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:15:22 up  1:12,  1 user,  load average: 0.65, 0.43, 0.38 a19b4c8a9239b6c9663ebbba4a090054956a037

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-extern _Context* do_syscall(_Context*c);
+_Context* do_syscall(_Context*c);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,9 +7,9 @@ _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   printf("GPR1 is %d\n",c->GPR1);
   a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[3] = c->GPR3;
-  a[4] = c->GPR4;
+  //a[1] = c->GPR2;
+  //a[3] = c->GPR3;
+  //a[4] = c->GPR4;
 
   switch (a[0]) {
     case SYS_yield:_yield();c->GPRx=0;break;
