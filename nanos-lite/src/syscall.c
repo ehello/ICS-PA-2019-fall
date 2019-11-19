f98f45156660dda25e66b005138c58bc837ee1d2
tracer-ics2019 <tracer@njuics.org>
2019-11-19 21:18:30 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:18:30 up 15 min,  1 user,  load average: 0.59, 0.62, 0.50 74e0287af38ce7f595ca91f11e7ecff9c324618b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,14 @@
 #include "common.h"
 #include "syscall.h"
 
+extern void _yield();
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_yield:_yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
