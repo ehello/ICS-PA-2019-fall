4896e4e008ebabe45f3e7e99ffbe57034a7a33d2
tracer-ics2019 <tracer@njuics.org>
2019-11-19 09:34:43 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:34:43 up 12:46,  1 user,  load average: 1.69, 1.21, 0.95 9adf4e765f859f8abbbfd30362087396b1887b0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,12 +24,15 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(ehdr,0,sizeof(Elf_Ehdr));
   //printf("type is %d\n",temp.e_type);
   //printf("entry is %d\n",temp.e_entry);
-  printf("pht num is %d\n",temp.e_phnum);
+  
+  //printf("pht num is %d\n",temp.e_phnum);
+  printf("e_entry is %d\n",temp.e_entry);
   Elf_Phdr pht[temp.e_phnum];
   ramdisk_read(pht,temp.e_ehsize,sizeof(Elf_Phdr)*temp.e_phnum);
   for(int i = 0;i<temp.e_phnum;i++){
     printf("p_type is %d\n",pht[i].p_type);
     if(pht[i].p_type == PT_LOAD){
+      printf("vaddr is %d,paddr is %d\n ",pht[i].p_vaddr,pht[i].p_paddr);
       ramdisk_read((void*)pht[i].p_vaddr,pht[i].p_offset,pht[i].p_memsz);
       memset((void*)(pht[i].p_vaddr+pht[i].p_filesz),0,pht[i].p_memsz-pht[i].p_filesz);
     }
