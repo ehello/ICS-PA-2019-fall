dd342cf511be6645cf3d77623a9eab575209ed44
tracer-ics2019 <tracer@njuics.org>
2019-11-19 23:32:23 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:32:23 up  2:29,  1 user,  load average: 0.07, 0.06, 0.10 2005c789e345181750e32a5bd979297e1942eb20

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -41,9 +41,7 @@
 #endif
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
-  //printf("runnign the _syscall_\n");
-  //printf("type is %x\n",type);
-  //assert(type == 1);
+  assert(type != 1);
   register intptr_t _gpr1 asm (GPR1) = type;
   register intptr_t _gpr2 asm (GPR2) = a0;
   register intptr_t _gpr3 asm (GPR3) = a1; 
