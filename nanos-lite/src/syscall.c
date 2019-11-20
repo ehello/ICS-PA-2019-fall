84cc70b128e23472e0a666fef14b33e8883fafeb
tracer-ics2019 <tracer@njuics.org>
2019-11-20 19:11:09 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:11:09 up  2:59,  1 user,  load average: 0.58, 0.62, 0.57 30a7c20ea06da8474a8057df53d37ad2d754e8d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,12 +7,12 @@ extern void _exit(int status);
 
 _Context* do_syscall(_Context* c) {
   uintptr_t a[4];
-  printf("GPR1 is %d\n",c->GPR1);
+  //printf("GPR1 is %d\n",c->GPR1);
   a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
-  a[3] = c->GPR4;
-  a[4] = c->GPRx;
+  //a[1] = c->GPR2;
+  //a[2] = c->GPR3;
+  //a[3] = c->GPR4;
+  //a[4] = c->GPRx;
 
   switch (a[0]) {
     case SYS_yield:_yield();c->GPRx=0;break;
--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -5,11 +5,10 @@ struct _Context {
   //根据trap.S,首先是存储通用寄存器，然后是地址空间，然后是事件号，然后是eflags、cs和eip
   //所以要把通用寄存器和eflags等三个寄存器分开放置
   uintptr_t  edi,esi,ebp,esp,ebx,edx,ecx,eax;
+  uintptr_t  eip,cs,eflags;
   struct _AddressSpace *as;
   int irq;
-  uintptr_t eip,cs,eflags;
-  //uintptr_t eflags,cs,eip;
-  //uintptr_t GPR1,GPR2,GPR3,GPR4,GPRx;
+  //uintptr_t eip,cs,eflags;
 };
 
 #define GPR1 eax 
