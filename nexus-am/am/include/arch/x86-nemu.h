e5418db80d883548f7863a7bfc4e0b25be26448a
tracer-ics2019 <tracer@njuics.org>
2019-11-20 20:30:59 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:30:59 up  4:19,  1 user,  load average: 0.68, 0.58, 0.57 3acef31a3d655ac33dee6b2c5023f93517c87ae5

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -1,14 +1,13 @@
 #ifndef __ARCH_H__
 #define __ARCH_H__
-
-struct _Context { 
+struct _Context {
   //根据trap.S,首先是存储通用寄存器，然后是地址空间，然后是事件号，然后是eflags、cs和eip
   //所以要把通用寄存器和eflags等三个寄存器分开放置
-  uintptr_t  edi,esi,ebp,esp,ebx,edx,ecx,eax;
   struct _AddressSpace *as;
+  uintptr_t  edi,esi,ebp,esp,ebx,edx,ecx,eax;
   int irq;
   uintptr_t eip,cs,eflags;
-};
+} ;
 
 #define GPR1 eax 
 #define GPR2 ebx
