3ea4103348b83bef304e9238f5d679fefa521123
tracer-ics2019 <tracer@njuics.org>
2019-11-21 09:53:39 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:53:39 up  1:14,  1 user,  load average: 0.55, 0.53, 0.54 24eae8a2bf8e8259eb71a0a41ec2feac8d33946

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,7 @@
 #include "syscall.h"
 //#include "/home/james/ics2019/nexus-am/am/am.h"
 
+extern void _putc(char ch);
 
 _Context* do_syscall(_Context* c) {
   uintptr_t a[4];
@@ -14,10 +15,16 @@ _Context* do_syscall(_Context* c) {
   switch (a[0]) {
     case SYS_yield:_yield(); c->GPRx = 0; break;
     case SYS_exit:_halt(a[1]); break;//may not correct
-    //case SYS_write:{
-    //    
-    //    _write();
-    //  }break; 
+    case SYS_write:{
+        if (a[1] == 1 || a[1] == 2){
+          int i = 0;
+          char *p = (char *)a[2];
+          for (;i<a[3];i++)
+            _putc(*p+i);
+          c->GPRx = i;
+        }
+        else c->GPRx = -1;
+      }break; 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
