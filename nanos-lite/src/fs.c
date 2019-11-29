0c5ec9747cd52d7511ec5f7fe85cc8c7b002f15f
tracer-ics2019 <tracer@njuics.org>
2019-11-29 21:38:54 +0800

    >  run 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:38:54 up  1:34,  1 user,  load average: 0.16, 0.37, 0.52 9fab9125b02d5da380d5394c69c59575a8e0f809

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,7 +76,7 @@ __ssize_t fs_read(int fd, void *buf, size_t len){//返回值类型？
     case FD_FB:
       break;
     case FD_EVENTS: ret = file_table[fd].read(buf,file_table[fd].open_offset,len);break;
-    case FD_FBSYNC: break;
+    //case FD_FBSYNC: break;
     case FD_DISPINFO:{
       if(file_table[fd].open_offset >= file_table[fd].size) 
         return 0;
