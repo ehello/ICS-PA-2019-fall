4fd09fc7fc136911d21c9bb7501e635896f51591
tracer-ics2019 <tracer@njuics.org>
2019-12-17 22:07:07 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:07:07 up  7:25,  1 user,  load average: 0.30, 0.12, 0.10 4dccf61815e07d178fdfe3d0114ed5f424a69be5

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,10 +1,40 @@
 #include "nemu.h"
 
+paddr_t page_translate(vaddr_t addr){
+  //见手册图5.8
+  paddr_t dir = (addr >> 22) & 0x3ff;//vaddr的22-31位,共10位
+  paddr_t page = (addr >> 12) & 0x3ff;//vaddr的12-21位，共10位
+  paddr_t offset = addr & 0xfff;//vaddr的0-11位，共12位
+
+  if(cpu.cr0.paging){//CR0的PG位为1时，开启分页模式
+    uint32_t pg_dir_base = cpu.cr3.page_directory_base;
+    uint32_t pt = paddr_read((pg_dir_base << 12)+(dir << 2), 4);
+    assert(pt & 1);//检查present位
+    uint32_t pf = paddr_read((pt & 0xfffff000)+(page<<2),4);
+    assert(pf & 1);
+
+    return (pf & 0xfffff000) + offset;
+
+  }
+  return addr;//未开启分页模式，直接返回虚拟地址
+}
+
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
- 
+  //return paddr_read(addr, len);
+  if((addr & 0xfff)+len > 0x1000 ){
+    assert(0);
+  }
+  else{
+    paddr_t paddr = page_translate(addr);
+    return paddr_read(paddr,len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  //paddr_write(addr, data, len);
+  if((addr & 0xfff)+len > 0x1000 ){
+    assert(0);
+  }
+  else
+   paddr_write(page_translate(addr), data, len);
 }
