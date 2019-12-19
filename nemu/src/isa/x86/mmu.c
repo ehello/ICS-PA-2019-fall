fcd959d4457bf775e6149bf24f383817391362cc
tracer-ics2019 <tracer@njuics.org>
2019-12-19 19:14:13 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:14:13 up  5:20,  1 user,  load average: 0.84, 0.78, 0.65 6e5846042617571f999c6405e3ba04c5e97c1e8e

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -17,20 +17,20 @@ paddr_t page_translate(vaddr_t addr){
 
   }
   return addr;//未开启分页模式，直接返回虚拟地址*/
+
   paddr_t dir = (addr >> 22) & 0x3ff;//vaddr的22-31位,共10位
   paddr_t page = (addr >> 12) & 0x3ff;//vaddr的12-21位，共10位
   paddr_t offset = addr & 0xfff;//vaddr的0-11位，共12位
-  if (cpu.cr0.paging){
-    uint32_t pg_dir_base = cpu.cr3.page_directory_base;
-    paddr_t ptab = paddr_read(pg_dir_base + sizeof(PDE) * dir, sizeof(PDE));
-    assert(ptab & 0x001);
+ 
+  paddr_t ptab = paddr_read(cpu.cr3.val + sizeof(PDE) * dir, sizeof(PDE));
+  assert(ptab & 0x001);
 
-    paddr_t pg = paddr_read((ptab & 0xfffff000) + sizeof(PTE) * page, sizeof(PTE));
-    assert(pg & 0x001);
+  paddr_t pg = paddr_read((ptab & 0xfffff000) + sizeof(PTE) * page, sizeof(PTE));
+  assert(pg & 0x001);
 
-    return ((pg & 0xfffff000) | offset);
-  }
-  return addr;
+  return ((pg & 0xfffff000) | offset);
+  
+ 
   
 }
 
@@ -39,21 +39,21 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
   //if((addr & 0xfff)+len > 0x1000 ){
   //  assert(0);
   //}
-  if((addr & ~0xfff) != ((addr + len - 1) & ~0xfff)){
-    uint8_t temp[4];
-    for (int i = 0; i < len; i++)
-      temp[i] = isa_vaddr_read(addr + i, 1);
-    if (len == 2)
-      return *(uint16_t *)temp;
+  if(cpu.cr0.paging){
+    if((addr & 0xfffff000) != ((addr + len - 1) & 0xfffff000)){
+      uint8_t temp[4];
+      for (int i = 0; i < len; i++)
+        temp[i] = isa_vaddr_read(addr + i, 1);
+      if (len == 2)
+        return *(uint16_t *)temp;
+      else
+        return *(uint32_t *)temp;
+    }
     else
-      return *(uint32_t *)temp;
+      return paddr_read(page_translate(addr), len);
   }
-  else{
-    paddr_t paddr = page_translate(addr);
-    return paddr_read(paddr,len);
-  }
-  //paddr_t paddr = page_translate(addr);
-  //return paddr_read(paddr,len);
+  else
+    return paddr_read(addr, len);
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
@@ -61,15 +61,19 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   //if((addr & 0xfff)+len > 0x1000 ){
   //  assert(0);
   //}
-  if ((addr & ~0xfff) != ((addr + len - 1) & ~0xfff)){
-    int8_t temp[4];
-    if (len == 2)
-      *(uint16_t *)temp = data;
+  if(cpu.cr0.paging){
+    if ((addr & 0xfffff000) != ((addr + len - 1) & 0xfffff000)){
+      int8_t temp[4];
+      if (len == 2)
+        *(uint16_t *)temp = data;
+      else
+        *(uint32_t *)temp = data;
+      for (int i = 0; i < len; i++)
+        isa_vaddr_write(addr + i, temp[i], 1);
+    }
     else
-      *(uint32_t *)temp = data;
-    for (int i = 0; i < len; i++)
-      isa_vaddr_write(addr + i, temp[i], 1);
+      paddr_write(page_translate(addr), data, len);
   }
   else
-   paddr_write(page_translate(addr), data, len);
+    paddr_write(addr, data, len);
 }
