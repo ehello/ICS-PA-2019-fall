5ee742a3b4306d44d68bb8703b5a36809cbadd99
tracer-ics2019 <tracer@njuics.org>
2019-12-22 15:50:10 +0800

    >  compile 181220039 james Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:50:10 up  6:38,  1 user,  load average: 0.64, 0.45, 0.24 44363fd7a509e2cd9e81cc625ac7f401a96d539

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -184,7 +184,7 @@ void context_kload(PCB *pcb, void *entry) {
 
 void context_uload(PCB *pcb, const char *filename) {
   _protect(&(pcb->as));
-  printf("The address space is %x\n",(uintptr_t)((pcb->as).ptr));
+  //printf("The address space is %x\n",(uintptr_t)((pcb->as).ptr));
   uintptr_t entry = loader(pcb, filename);
 
   _Area stack;
